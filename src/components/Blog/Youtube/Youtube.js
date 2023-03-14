a5368ad7d2654982e1583721141b15aa8a3eb91d
huyjaky <huyphanuyg@gmail.com>
2023-03-14 15:32:48 +0700

    search bar

--- src/components/Blog/Youtube/Youtube.js
+++ src/components/Blog/Youtube/Youtube.js
@@ -1,23 +1,27 @@
+import ShowRS from './showRS/ShowRS';
 import './Style.scss';
 
 const Youtube = () => {
   return (
-    <form>
-      <label htmlFor="Search" className="form-label">
-        Search Bar
-      </label>
-      <div className="mb-3 form-search">
-        <input
-          type="email"
-          className="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
-        />
-        <button type="submit" className="btn btn-light" style={{ borderRadius: '20px' }}>
-          Submit
-        </button>
-      </div>
-    </form>
+    <>
+      <form>
+        <label htmlFor="Search" className="form-label">
+          Search Bar
+        </label>
+        <div className="mb-3 form-search">
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+          <button type="submit" className="btn btn-light" style={{ borderRadius: '20px' }}>
+            Submit
+          </button>
+        </div>
+      </form>
+      <ShowRS />
+    </>
   );
 };
 
---
+++ src/components/Blog/Youtube/showRS/ShowRS.js
@@ -0,0 +1,33 @@
+
+const ShowRS = () => {
+  return (
+    <>
+      <div>
+        <div className="card mb-3">
+          <div className="row g-0">
+            <div className="col-md-4">
+              <iframe
+                width='850'
+                height='480'
+                src="https://www.youtube.com/embed/YbmvFEXZG7Q?list=PLncHg6Kn2JT4xzJyhXfmJ53dzwVbq-S_E"
+                title="#30.2 Design Giao Diện &amp; Hoàn Thiện Chức Năng &#39;Search Youtube&#39; với Google APIs và React Hook"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowFullscreen
+              ></iframe>
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default ShowRS;
