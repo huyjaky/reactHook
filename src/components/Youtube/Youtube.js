7b94ec4f6a140deaa3217a9d6f3a6c2ec7c57e73
huyjaky <huyphanuyg@gmail.com>
2023-03-14 19:10:12 +0700

    youtube api

--- src/components/Youtube/Youtube.js
+++ src/components/Youtube/Youtube.js
@@ -1,27 +1,51 @@
+import axios from 'axios';
+
+import { useState } from 'react';
 import ShowRS from './showRS/ShowRS';
 import './Style.scss';
 
 const Youtube = () => {
+
+  const [search, setSearch] = useState('');
+  const [data, setData] = useState();
+
+
+  const handleYoutubeRs = async () => {
+    let res = await axios({
+      "method": "GET",
+      "url": 'https://www.googleapis.com/youtube/v3/search',
+      "params": {
+        'part': 'snippet',
+        'maxResults': '50',
+        'key': 'AIzaSyCHk1sW7naiONdzG0h2iH99W2ivIr529K4',
+        'q': search
+      }
+    });
+    await setData(res.data);
+    console.log(data);
+  }
+
   return (
     <>
       <form>
         <label htmlFor="Search" className="form-label">
           Search Bar
         </label>
-        <div className="mb-3 form-search" style={{width: '30vw'}}>
-            <input
-              type="email"
-              className="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-              placeholder='Search'
-            />
-          <button type="submit" className="btn btn-primary" style={{ borderRadius: '20px' }}>
+        <div className="mb-3 form-search" style={{ width: '30vw' }}>
+          <input
+            type="text"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="Search"
+            onChange={(event) => setSearch(event.target.value)}
+          />
+          <button type="button" className="btn btn-primary" style={{ borderRadius: '20px' }} onClick={() => handleYoutubeRs()}>
             Submit
           </button>
         </div>
       </form>
-      <ShowRS />
+      <ShowRS data={data}/>
     </>
   );
 };
--- src/components/Youtube/showRS/ShowRS.js
+++ src/components/Youtube/showRS/ShowRS.js
@@ -1,34 +1,53 @@
 import './Style.scss';
+// import PropTypes from 'prop-types';
 
-const ShowRS = () => {
+const ShowRS = (props) => {
+
+  // const { data } = props;
+
   return (
     <>
-      <div style={{width: '80vw'}}>
-        <div className="card mb-3 bg-transparent">
-          <div className="row g-0  layout-rs" >
-            <div className="col-md-4">
-              <iframe
-                width='850'
-                height='480'
-                src="https://www.youtube.com/embed/YbmvFEXZG7Q?list=PLncHg6Kn2JT4xzJyhXfmJ53dzwVbq-S_E"
-                title="#30.2 Design Giao Diện &amp; Hoàn Thiện Chức Năng &#39;Search Youtube&#39; với Google APIs và React Hook"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                allowFullscreen
-              ></iframe>
-            </div>
-            <div className="col-md-8">
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+      {/* {data && */}
+        <div style={{ width: '80vw' }}>
+          <div className="card mb-3 bg-transparent">
+            <div className="row g-0  layout-rs">
+              <div className="col-md-4">
+                <iframe
+                  width="850"
+                  height="480"
+                  // src="https://www.youtube.com/embed/YbmvFEXZG7Q?list=PLncHg6Kn2JT4xzJyhXfmJ53dzwVbq-S_E"
+                  src="https://www.youtube.com/embed/KnsjOS95LLM"
+                  title="#30.2 Design Giao Diện &amp; Hoàn Thiện Chức Năng &#39;Search Youtube&#39; với Google APIs và React Hook"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  allowFullScreen
+                ></iframe>
+              </div>
+              <div
+                className="col-md-8"
+                style={{ backgroundColor: 'white', borderRadius: '20px', marginLeft: '20px' }}
+              >
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    This is a wider card with supporting text below as a natural lead-in to additional
+                    content. This content is a little bit longer.
+                  </p>
+                  <p className="card-text">
+                    <small className="text-muted">Last updated 3 mins ago</small>
+                  </p>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      {/* } */}
     </>
-  )
-}
+  );
+};
 
 export default ShowRS;
+
+// ShowRS.propTypes = {
+//   data: PropTypes.array
+// }
